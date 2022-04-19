54ff13cd2c2f34726bec6d0a9a3f81a3fee769db
David Gabriel <uselessnebula2@outlook.com>
2022-04-18 21:33:20 -0400

    please work

--- app.py
+++ app.py
@@ -40,9 +40,11 @@ def submitData():
     disconnected_total_seconds = None
     try:
         throwaway_var4 = data["disconnected_total_seconds"]
-        disconnected_total_seconds = int(data["disconnected_total_seconds"])
+        disconnected_total_seconds = True
     except KeyError:
-        disconnected_total_seconds = int(False)
+        disconnected_total_seconds = False
+    if disconnected_total_seconds == true:
+        disconnected_total_seconds = int(data["disconnected_total_seconds"])
     data = {  # to clear things up, this data is the data of a single match
         "team_number": team,
         "match": int(data["match"]),
