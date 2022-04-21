fc41d571fe32f00f054faf1ca7d5cbda7f9caa54
uni-mairata <maika.h@prhsrobotics.com>
2022-04-20 23:36:19 -0400

    Revert "Formatting edits again"
    This reverts commit 71190f14f3e3261a2d336b2c5fcafe56d547e06f.

--- forms.py
+++ forms.py
@@ -38,7 +38,7 @@ class InfiniteRechargeForm(FlaskForm):
 
 class RapidReactForm(FlaskForm):
     team_number = IntegerField("Team Number", validators=[DataRequired(), NumberRange(1, 20000)])
-    match = StringField("Name of Match", validators=[DataRequired()], widget=TextArea())
+    match_name = StringField("Name of Match", validators=[DataRequired()], widget=TextArea())
     disabled = BooleanField("Disabled/AFK")
     disconnected = BooleanField("Disconnected during the match")
     disconnected_total_seconds = IntegerField("Total seconds disconnected", validators=[DataRequired()], default=0)
